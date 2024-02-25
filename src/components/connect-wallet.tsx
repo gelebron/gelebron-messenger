db444c6b954c8ed74c11c225d64c56cb9686b125
Dann Wee <dannweezj@gmail.com>
2024-02-25 10:53:51 +0800

    improved wallet connect button

--- src/components/connect-wallet.tsx
+++ src/components/connect-wallet.tsx
@@ -5,9 +5,11 @@ import { WalletContext } from "@/providers/WalletContext";
 import { XmtpContext } from "@/providers/XmtpContext";
 import { WalletIcon } from "lucide-react";
 import React, { useContext } from "react";
+import { Button } from "./ui/button";
 
 const ConnectWallet = () => {
-  const { connectWallet, walletAddress, signer } = useContext(WalletContext);
+  const { connectWallet, disconnectWallet, walletAddress, signer } =
+    useContext(WalletContext);
   const [providerState] = useContext(XmtpContext) as [
     any,
     React.Dispatch<React.SetStateAction<any>>
@@ -19,31 +21,38 @@ const ConnectWallet = () => {
     window.ethereum.isMetaMask;
 
   return (
-    <div className="flex justify-center items-center text-sm px-2 md:text-md md:space-y-0 md:space-x-4 cursor-pointer border py-3 rounded-lg">
-      <WalletIcon className="hidden md:flex h-4 w-4" />
+    <div className="flex justify-center items-center text-sm px-2 md:text-md md:space-y-0 md:space-x-4 cursor-pointer py-3 rounded-lg">
       {walletAddress ? (
         <>
           {!providerState.client ? (
-            <span
+            <Button
               onClick={() => providerState.initClient(signer)}
-              className="flex justify-center items-center"
+              className="flex justify-center items-center gap-3"
             >
+              <WalletIcon className="hidden md:flex h-4 w-4" />
               Connect to XMTP
-            </span>
+            </Button>
           ) : (
-            <p className="">{shortAddress(walletAddress)}</p>
+            <Button
+              className="flex justify-center items-center gap-3"
+              onClick={disconnectWallet}
+            >
+              <WalletIcon className="hidden md:flex h-4 w-4" />
+              {shortAddress(walletAddress)}
+            </Button>
           )}
         </>
       ) : (
-        <span
-          className="flex justify-center items-center"
+        <Button
+          className="flex justify-center items-center gap-3"
           onClick={connectWallet}
         >
+          <WalletIcon className="hidden md:flex h-4 w-4 gap-2" />
           {typeof window !== "undefined" &&
           (!window.ethereum || !window.ethereum.isMetaMask)
             ? "Install MetaMask"
-            : "Connect wallet"}
-        </span>
+            : "Connect Wallet"}
+        </Button>
       )}
     </div>
   );
